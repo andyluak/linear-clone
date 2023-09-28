49300ee264ba0b5d625502a0891c60cfd96a5d97
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2023-09-28 22:23:21 +0300

    .

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import clsx from "clsx";
 
 type Props = {
   children: React.ReactNode;
+  className?: string;
 };
 
-const Container = ({ children }: Props) => {
-  return <div className="max-w-[1200px] mx-auto">{children}</div>;
+const Container = ({ children, className }: Props) => {
+  return (
+    <div className={clsx("mx-auto max-w-[1200px]", className)}>{children}</div>
+  );
 };
 
 export default Container;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,34 @@
+import Link from "next/link";
 import Container from "./Container";
+import { Logo } from "./icons/logo";
+
+const navItems = [
+  { name: "Features", href: "/features" },
+  { name: "Method", href: "/method" },
+  { name: "Customers", href: "/customers" },
+  { name: "Changelog", href: "/changelog" },
+  { name: "Integration", href: "/integration" },
+  { name: "Pricing", href: "/pricing" },
+  { name: "Company", href: "/company" },
+];
 
 const Header = () => (
   <header>
-    <Container>header</Container>
+    <Container className="flex h-[var(--navigation-height)] items-center">
+      <Link href="/">
+        <Logo />
+      </Link>
+
+      <nav>
+        <ul className="flex">
+          {navItems.map((item) => (
+            <Link href={item.href} key={item.name}>
+              <li>{item.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+    </Container>
   </header>
 );
 
